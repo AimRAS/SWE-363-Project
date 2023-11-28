ac779a8395010aaa5657493769e47d38b81ee1f6
Abdullah Sahwan <abdullah.sahwan.2021@gmail.com>
2023-11-28 01:04:06 +0000

    desktop version of main page

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// Add a click event listener to the button
+document.getElementById('AddEvent').addEventListener('click', function() {
+    // Redirect to another HTML file
+    window.location.href = 'html/AddEvent.html';
+    console.log(234);
+});
+   
